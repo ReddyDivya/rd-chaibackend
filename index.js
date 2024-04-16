17d21e1ab209ed823bceaaa2715fc25cb4aaf5e2
Divya <divyareddy0205@gmail.com>
2024-04-16 14:18:27 -0700

    setup server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 4000;
@@ -57,6 +58,6 @@ app.get('/github', (req, res) => {
     res.json(githubData);//sending json object
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
 })
